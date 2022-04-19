1054df30aae527d1a0bfb932a819bf3f9ed4313b
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-04-19 13:27:36 +0100

    feat: product and tag APIs

--- src/un1qnx/__init__.py
+++ src/un1qnx/__init__.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+from . import base
+from . import product
+from . import tag
+
+from .base import API
+from .product import ProductAPI
+from .tag import TagAPI
--- src/un1qnx/base.py
+++ src/un1qnx/base.py
@@ -3,14 +3,21 @@
 
 import appier
 
-UN1QNX_BASE_URL = "http://un1qone-backend-test.azurewebsites.net/api/"
+from . import tag
+from . import product
+
+UN1QNX_BASE_URL = "http://un1qone-backend-test.azurewebsites.net/api/v2/"
 """ The default base URL to be used when no other
 base URL value is provided to the constructor """
 
 class API(
-    appier.API
+    appier.API,
+    tag.TagAPI,
+    product.ProductAPI
 ):
 
     def __init__(self, *args, **kwargs):
         appier.API.__init__(self, *args, **kwargs)
         self.base_url = appier.conf("UN1QNX_BASE_URL", UN1QNX_BASE_URL)
+
+        self.base_url = kwargs.get("base_url", self.base_url)
