e2b2361d3fd6832f474935a49e63ff01e7fd658d
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-04-19 13:33:35 +0100

    feat: different auth endpoint

--- src/un1qnx/base.py
+++ src/un1qnx/base.py
@@ -10,6 +10,10 @@ UN1QNX_BASE_URL = "http://un1qone-backend-test.azurewebsites.net/api/v2/"
 """ The default base URL to be used when no other
 base URL value is provided to the constructor """
 
+UN1QNX_AUTH_URL = "http://un1qone-identity-server-test.azurewebsites.net/"
+""" The default auth URL to be used when no other
+auth URL value is provided to the constructor """
+
 class API(
     appier.API,
     tag.TagAPI,
@@ -19,5 +23,7 @@ class API(
     def __init__(self, *args, **kwargs):
         appier.API.__init__(self, *args, **kwargs)
         self.base_url = appier.conf("UN1QNX_BASE_URL", UN1QNX_BASE_URL)
+        self.auth_url = appier.conf("UN1QNX_AUTH_URL", UN1QNX_AUTH_URL)
 
         self.base_url = kwargs.get("base_url", self.base_url)
+        self.auth_url = kwargs.get("auth_url", self.auth_url)
