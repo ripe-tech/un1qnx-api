09115dc15d6f9c1cd6f6bf3f9ad9ddfd31e3f820
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-04-22 12:32:27 +0100

    version: 0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = "un1qnx-api",
-    version = "0.0.1",
+    version = "0.1.0",
     author = "Platforme International",
     author_email = "development@platforme.com",
     description = "un1qnx API",
